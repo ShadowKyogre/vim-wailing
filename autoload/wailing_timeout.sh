7c7b8dc8713902626d97c38c1e2151b19bdc5dce
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-08-17 14:58:57 -0700

    Mandatory file exist and socket check
    Otherwise, we'll clobber files that're supposed to be the alarms.

--- autoload/wailing_timeout.sh
+++ autoload/wailing_timeout.sh
@@ -5,8 +5,15 @@ if [ $# -lt 2 ]; then
 	exit 1
 fi
 
+if [ -e "$2" -a ! -S "$2" ]; then
+	>&2 echo "Cannot use existing file as socket!"
+	exit 1
+fi
+
 echo $$
 sleep "$1"
+
+
 echo "stop"|socat - "$2"
 
 if [ $#  -eq 3 ]; then
